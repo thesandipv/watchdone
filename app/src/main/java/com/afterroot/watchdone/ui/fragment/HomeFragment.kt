a378e340299814c9aa41d873d0f01c483fe17527
Sandip <sandipvaghela311@gmail.com>
2020-05-01 13:53:57 +0530

    init ui, new tmdbapi module

--- app/src/main/java/com/afterroot/watchdone/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/afterroot/watchdone/ui/fragment/HomeFragment.kt
@@ -15,22 +15,99 @@
 
 package com.afterroot.watchdone.ui.fragment
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.DatePicker
+import android.widget.TimePicker
 import androidx.fragment.app.Fragment
-
+import androidx.lifecycle.lifecycleScope
+import com.afollestad.materialdialogs.LayoutMode
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.bottomsheets.BottomSheet
+import com.afollestad.materialdialogs.customview.customView
+import com.afollestad.materialdialogs.customview.getCustomView
 import com.afterroot.watchdone.R
+import kotlinx.android.synthetic.main.content_add_watched.view.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.GregorianCalendar
+import java.util.Locale
 
-class HomeFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+class HomeFragment : Fragment(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+    lateinit var dialogCustomView: View
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        button_action_add_watched.setOnClickListener {
+            MaterialDialog(requireContext(), BottomSheet(LayoutMode.WRAP_CONTENT)).show {
+                title(text = "Add Watched")
+                customView(R.layout.content_add_watched)
+                dialogCustomView = getCustomView().apply {
+                    text_input_date.setOnClickListener {
+                        showDatePicker()
+                    }
+                }
+                positiveButton(text = "Add") {
+                    lifecycleScope.launch {
+                        //TODO Add Task
+                    }
+                }
+            }
+        }
+    }
+
+    private lateinit var cal: Calendar
+    private var day: Int? = 0
+    private var hourOfDay: Int? = 0
+    private var millis: Long = 0
+    private var minute: Int? = 0
+    private var month: Int? = 0
+    private var pickedDate: GregorianCalendar? = null
+    private var year: Int? = 0
+
+    private fun showDatePicker() {
+        cal = Calendar.getInstance()
+        day = cal.get(Calendar.DAY_OF_MONTH)
+        hourOfDay = cal.get(Calendar.HOUR_OF_DAY)
+        minute = cal.get(Calendar.MINUTE)
+        month = cal.get(Calendar.MONTH)
+        year = cal.get(Calendar.YEAR)
+        val datePicker = DatePickerDialog(requireContext(), this, year!!, month!!, day!!)
+        datePicker.show()
+    }
+
+    private fun showTimePicker() {
+        val picker = TimePickerDialog(this.context, this, hourOfDay!!, minute!!, false)
+        picker.show()
+    }
+
+    override fun onDateSet(p0: DatePicker?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+        this.year = year
+        this.month = monthOfYear
+        this.day = dayOfMonth
+        showTimePicker()
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        pickedDate = GregorianCalendar(year!!, month!!, day!!, hourOfDay, minute)
+        millis = pickedDate!!.timeInMillis
+
+        val formatter = SimpleDateFormat(getString(R.string.date_time_format), Locale.US)
+        dialogCustomView.text_input_date.text = formatter.format(Date(millis))
+    }
+
+    companion object {
+        private const val TAG = "HomeFragment"
     }
 }
