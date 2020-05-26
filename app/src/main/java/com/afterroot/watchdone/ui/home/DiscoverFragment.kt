28eb9fe699f7a05f0c541ad032ac6b42bc9c4059
Sandip <sandipvaghela311@gmail.com>
2020-05-26 15:10:24 +0530

    new DiscoverFragment and Genres in MovieInfoFragment

--- app/src/main/java/com/afterroot/watchdone/ui/home/DiscoverFragment.kt
+++ app/src/main/java/com/afterroot/watchdone/ui/home/DiscoverFragment.kt
@@ -16,15 +16,37 @@
 package com.afterroot.watchdone.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.afterroot.tmdbapi.model.Discover
+import com.afterroot.tmdbapi2.repository.DiscoverRepository
+import com.afterroot.watchdone.R
 import com.afterroot.watchdone.databinding.FragmentDiscoverBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.get
 
 class DiscoverFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentDiscoverBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        requireActivity().toolbar.replaceMenu(R.menu.menu_discover)
+
+        lifecycleScope.launch {
+            val repo = DiscoverRepository(get()).getMoviesDiscover(Discover())
+            Log.d(TAG, "onActivityCreated: ${repo.results}")
+        }
+    }
+
+    companion object {
+        private const val TAG = "DiscoverFragment"
     }
 }
