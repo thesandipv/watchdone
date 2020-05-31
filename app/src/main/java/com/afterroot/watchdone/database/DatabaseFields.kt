955da82df4e65e1010e449cd54301d37df666ff1
Sandip <sandipvaghela311@gmail.com>
2020-05-31 22:13:09 +0530

    changed document path for watchlist

--- app/src/main/java/com/afterroot/watchdone/database/DatabaseFields.kt
+++ app/src/main/java/com/afterroot/watchdone/database/DatabaseFields.kt
@@ -19,8 +19,11 @@ object DatabaseFields {
     //table 'users'
     const val COLLECTION_USERS = "users"
     const val COLLECTION_WATCHDONE = "watchdone"
+    const val COLLECTION_WATCHLIST = "watchlist"
+    const val COLLECTION_ITEMS = "items"
     const val FIELD_NAME = "name"
     const val FIELD_EMAIL = "email"
     const val FIELD_UID = "uid"
     const val FIELD_FCM_ID = "fcmId"
+    const val FIELD_TOTAL_ITEMS = "total_items"
 }
