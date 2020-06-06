aed38c02e708a5099e569d61403543f6717fbc14
Sandip <sandipvaghela311@gmail.com>
2020-06-06 17:59:56 +0530

    Add Testing method for Movie Response

---
+++ app/src/androidTest/java/com/afterroot/watchdone/MovieInfoTest.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2020 Sandip Vaghela
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.afterroot.watchdone
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.afterroot.tmdbapi2.repository.MoviesRepository
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.test.KoinTest
+import org.koin.test.get
+
+@RunWith(AndroidJUnit4::class)
+class MovieInfoTest : KoinTest {
+    @Test
+    fun isNewMovieDbWorking() {
+        runBlocking {
+            launch {
+                Assert.assertEquals("Fight Club", get<MoviesRepository>().getMovieInfo(550).title)
+            }
+        }
+    }
+}
