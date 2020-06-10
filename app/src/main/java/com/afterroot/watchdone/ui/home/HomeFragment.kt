09d57b91ecb1251dbdd6445f03284d857ea88b6c
Sandip Vaghela <sandipvaghela311@gmail.com>
2020-06-10 11:01:51 +0530

    replaced search widget with SearchView

--- app/src/main/java/com/afterroot/watchdone/ui/home/HomeFragment.kt
+++ app/src/main/java/com/afterroot/watchdone/ui/home/HomeFragment.kt
@@ -17,6 +17,8 @@ package com.afterroot.watchdone.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
@@ -92,6 +94,11 @@ class HomeFragment : Fragment() {
                 homeViewModel.error.postValue(null)
             }
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     companion object {
