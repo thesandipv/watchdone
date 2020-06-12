c7e4b25974ef50ba6810a43b4a996d15f38505d4
Sandip Vaghela <sandipvaghela311@gmail.com>
2020-06-12 19:26:39 +0530

    error is handled by Event class

--- app/src/main/java/com/afterroot/watchdone/ui/home/HomeFragment.kt
+++ app/src/main/java/com/afterroot/watchdone/ui/home/HomeFragment.kt
@@ -33,6 +33,8 @@ import com.afterroot.watchdone.adapter.DelegateListAdapter
 import com.afterroot.watchdone.adapter.ItemSelectedCallback
 import com.afterroot.watchdone.adapter.MovieDiffCallback
 import com.afterroot.watchdone.databinding.FragmentHomeBinding
+import com.afterroot.watchdone.utils.EventObserver
+import com.google.android.material.chip.Chip
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.android.synthetic.main.fragment_search.*
@@ -86,13 +88,9 @@ class HomeFragment : Fragment() {
     }
 
     private fun setErrorObserver() {
-        homeViewModel.error.observe(viewLifecycleOwner, Observer {
-            if (it != null) {
-                progress_bar_search.visible(false, AutoTransition())
-                requireContext().toast("Via: $TAG : $it")
-                //Set value to null after displaying error so prevent Observers from another context
-                homeViewModel.error.postValue(null)
-            }
+        homeViewModel.error.observe(viewLifecycleOwner, EventObserver {
+            progress_bar_search.visible(false, AutoTransition())
+            requireContext().toast("Via: $TAG : $it")
         })
     }
 
