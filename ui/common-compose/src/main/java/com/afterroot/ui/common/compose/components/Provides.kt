1ad4548bb33478267f788b819d5690949d444dcf
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2021-09-29 20:03:30 +0530

    add search module few changes in AndroidManifest.xml supports Android 12

--- ui/common-compose/src/main/java/com/afterroot/ui/common/compose/components/Provides.kt
+++ ui/common-compose/src/main/java/com/afterroot/ui/common/compose/components/Provides.kt
@@ -4,5 +4,6 @@
 package com.afterroot.ui.common.compose.components
 
 import androidx.compose.runtime.compositionLocalOf
+import com.afterroot.watchdone.data.model.User
 
 val LocalCurrentUser = compositionLocalOf { User() }
