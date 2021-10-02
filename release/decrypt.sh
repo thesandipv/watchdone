0e15354bee1886c90b5aabc7f68d690f6f84242c
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2021-10-02 16:49:30 +0530

    add core as project

--- release/decrypt.sh
+++ release/decrypt.sh
@@ -17,4 +17,5 @@
 
 gpg --quiet --batch --yes --decrypt --passphrase=${ENCRYPT_KEY} --output release/keystore.jks release/keystore.gpg
 gpg --quiet --batch --yes --decrypt --passphrase=${ENCRYPT_KEY} --output release/serviceAccount.json release/serviceAccount.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=${ENCRYPT_KEY} --output core/core/publish.properties core/core/publish.properties.gpg
 #gpg --quiet --batch --yes --decrypt --passphrase=${ENCRYPT_KEY} --output app/google-services.json app/google-services.gpg
