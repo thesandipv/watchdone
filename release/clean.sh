1fadfa7d0a232a1ab594f75fc9e4ff1addab5fc9
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2021-10-02 16:49:30 +0530

    add core as project

--- release/clean.sh
+++ release/clean.sh
@@ -17,4 +17,5 @@
 
 rm -f ./release/keystore.jks
 rm -f ./release/serviceAccount.json
+rm -f ./core/core/publish.properties
 #rm -f ./app/google-services.json
--- release/encrypt.sh
+++ release/encrypt.sh
@@ -17,4 +17,5 @@
 
 gpg --passphrase=${ENCRYPT_KEY} --cipher-algo AES256 --symmetric --output release/keystore.gpg release/keystore.jks
 gpg --passphrase=${ENCRYPT_KEY} --cipher-algo AES256 --symmetric --output release/serviceAccount.gpg release/serviceAccount.json
+gpg --passphrase=${ENCRYPT_KEY} --cipher-algo AES256 --symmetric --output core/core/publish.properties.gpg core/core/publish.properties
 #gpg --passphrase=${ENCRYPT_KEY} --cipher-algo AES256 --symmetric --output app/google-services.gpg app/google-services.json
