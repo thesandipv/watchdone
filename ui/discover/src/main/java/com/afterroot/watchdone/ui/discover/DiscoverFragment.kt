facc7533e26cb4cb9987998f2d08534454015ab2
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2022-01-16 16:53:02 +0530

    add movie and tv button to discover

--- ui/discover/src/main/java/com/afterroot/watchdone/ui/discover/DiscoverFragment.kt
+++ ui/discover/src/main/java/com/afterroot/watchdone/ui/discover/DiscoverFragment.kt
@@ -21,24 +21,29 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavDeepLinkRequest
 import androidx.navigation.fragment.findNavController
 import com.afterroot.core.extensions.visible
-import com.afterroot.tmdbapi.model.Discover
 import com.afterroot.tmdbapi.model.Multi
 import com.afterroot.tmdbapi2.api.DiscoverApi
-import com.afterroot.tmdbapi2.repository.DiscoverRepository
-import com.afterroot.watchdone.data.mapper.toMovies
+import com.afterroot.ui.common.compose.components.TextChipGroup
+import com.afterroot.ui.common.compose.theme.Theme
 import com.afterroot.watchdone.data.model.Movie
-import com.afterroot.watchdone.diff.MovieDiffCallback
-import com.afterroot.watchdone.media.adapter.delegate.DelegateListAdapter
+import com.afterroot.watchdone.data.model.TV
+import com.afterroot.watchdone.media.adapter.MultiAdapter
 import com.afterroot.watchdone.settings.Settings
 import com.afterroot.watchdone.ui.common.ItemSelectedCallback
 import com.afterroot.watchdone.ui.discover.databinding.FragmentDiscoverBinding
+import com.afterroot.watchdone.viewmodel.DiscoverActions
+import com.afterroot.watchdone.viewmodel.DiscoverViewModel
 import com.afterroot.watchdone.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -51,11 +56,39 @@ import javax.inject.Named
 class DiscoverFragment : Fragment() {
     lateinit var binding: FragmentDiscoverBinding
     private val homeViewModel: HomeViewModel by activityViewModels()
+    private val discoverViewModel: DiscoverViewModel by viewModels()
     @Inject @Named("feedback_body") lateinit var feedbackBody: String
     @Inject lateinit var discoverApi: DiscoverApi
     @Inject lateinit var settings: Settings
+    private lateinit var discoverAdapter: MultiAdapter
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private val itemSelectedCallback = object : ItemSelectedCallback<Multi> {
+        override fun onClick(position: Int, view: View?, item: Multi) {
+            super.onClick(position, view, item)
+            if (item is Movie) {
+                val request = NavDeepLinkRequest.Builder
+                    .fromUri("https://watchdone.web.app/media/${Multi.MediaType.MOVIE.name}/${item.id}".toUri())
+                    .build()
+                findNavController().navigate(request)
+            } else if (item is TV) {
+                val request = NavDeepLinkRequest.Builder
+                    .fromUri("https://watchdone.web.app/media/${Multi.MediaType.TV_SERIES.name}/${item.id}".toUri())
+                    .build()
+                findNavController().navigate(request)
+            }
+        }
+
+        override fun onLongClick(position: Int, item: Multi) {
+            super.onLongClick(position, item)
+            if (item is Movie) {
+                requireContext().toast((item).title.toString())
+            } else if (item is TV) {
+                requireContext().toast((item).name.toString())
+            }
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         setHasOptionsMenu(true)
         binding = FragmentDiscoverBinding.inflate(inflater, container, false)
         return binding.root
@@ -64,9 +97,14 @@ class DiscoverFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        discoverAdapter = MultiAdapter(itemSelectedCallback, settings)
+
+        discoverViewModel.submitAction(DiscoverActions.SetMediaType(Multi.MediaType.MOVIE))
+
         lifecycleScope.launch {
             binding.progressBarDiscover.visible(true)
-            val repo = DiscoverRepository(discoverApi).getMoviesDiscover(Discover())
+            // val repo = DiscoverRepository(discoverApi).getMoviesDiscover(Discover())
+/*
             val homeScreenAdapter = DelegateListAdapter(
                 settings,
                 MovieDiffCallback(),
@@ -89,9 +127,33 @@ class DiscoverFragment : Fragment() {
                     }
                 }
             )
-            binding.list.adapter = homeScreenAdapter
-            homeScreenAdapter.submitList(repo.toMovies())
+*/
+            binding.list.adapter = discoverAdapter
+            // homeScreenAdapter.submitList(repo.toMovies())
             binding.progressBarDiscover.visible(false)
+            discoverViewModel.getMedia().collect {
+                discoverAdapter.submitList(it)
+            }
+        }
+
+        binding.composeViewChip.setContent {
+            Theme {
+                TextChipGroup(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    horizontalSpacing = 12.dp,
+                    chipModifier = Modifier.padding(horizontal = 4.dp, vertical = 2.dp),
+                    list = listOf("Movies", "TV")
+                ) { selected, _ ->
+                    when (selected) {
+                        "Movies" -> {
+                            discoverViewModel.submitAction(DiscoverActions.SetMediaType(Multi.MediaType.MOVIE))
+                        }
+                        "TV" -> {
+                            discoverViewModel.submitAction(DiscoverActions.SetMediaType(Multi.MediaType.TV_SERIES))
+                        }
+                    }
+                }
+            }
         }
     }
 
---
+++ ui/discover/src/main/java/com/afterroot/watchdone/viewmodel/DiscoverViewModel.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2020-2022 Sandip Vaghela
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.afterroot.watchdone.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.afterroot.tmdbapi.model.Discover
+import com.afterroot.tmdbapi.model.Multi
+import com.afterroot.tmdbapi2.repository.DiscoverRepository
+import com.afterroot.watchdone.base.compose.Actions
+import com.afterroot.watchdone.data.mapper.toMulti
+import com.afterroot.watchdone.utils.logD
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DiscoverViewModel @Inject constructor(
+    val savedState: SavedStateHandle? = null,
+    private val discoverRepository: DiscoverRepository
+) : ViewModel() {
+    private val actions = MutableSharedFlow<DiscoverActions>()
+    private val media = MutableSharedFlow<List<Multi>>()
+
+    init {
+        logD("DiscoverViewModel/Init", "Start")
+
+        viewModelScope.launch {
+            actions.collect { action ->
+                when (action) {
+                    is DiscoverActions.SetMediaType -> {
+                        when (action.mediaType) {
+                            Multi.MediaType.MOVIE -> {
+                                val list = discoverRepository.getMoviesDiscover(Discover()) // TODO add params also
+                                media.emit(list.toMulti())
+                            }
+                            Multi.MediaType.TV_SERIES -> {
+                                val list = discoverRepository.getTVDiscover(Discover()) // TODO add params also
+                                media.emit(list.toMulti())
+                            }
+                            else -> {}
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    internal fun submitAction(action: DiscoverActions) {
+        logD("DiscoverViewModel/submitAction", "Action: $action")
+        viewModelScope.launch {
+            actions.emit(action)
+        }
+    }
+
+    internal fun getAction() = actions
+
+    fun getMedia(): SharedFlow<List<Multi>> = media
+}
+
+sealed class DiscoverActions : Actions() {
+    data class SetMediaType(val mediaType: Multi.MediaType) : DiscoverActions()
+}
