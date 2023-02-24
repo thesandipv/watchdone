8a2cba892180bcf7e34d347a873f373d9cd32895
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2023-02-24 20:41:55 +0530

    remove version-updates plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,12 +31,12 @@ buildscript {
     }
 }
 
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.lint) apply false
     alias(libs.plugins.android.test) apply false
-    alias(libs.plugins.depUpdates)
     alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.gms.googleServices) apply false
     alias(libs.plugins.hilt) apply false
