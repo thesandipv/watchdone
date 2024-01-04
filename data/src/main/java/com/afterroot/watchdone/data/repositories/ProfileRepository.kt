47f78759b2e8c28a4d5555ec9e732dbf697333f0
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2024-01-05 00:17:46 +0530

    simplify code using resultFlow

--- data/src/main/java/com/afterroot/watchdone/data/repositories/ProfileRepository.kt
+++ data/src/main/java/com/afterroot/watchdone/data/repositories/ProfileRepository.kt
@@ -20,14 +20,12 @@ import com.afterroot.watchdone.data.mapper.toNetworkUser
 import com.afterroot.watchdone.data.model.LocalUser
 import com.afterroot.watchdone.utils.State
 import com.afterroot.watchdone.utils.collectionUsers
+import com.afterroot.watchdone.utils.resultFlow
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.Source
 import com.google.firebase.messaging.FirebaseMessaging
 import javax.inject.Inject
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.tasks.await
 
 class ProfileRepository @Inject constructor(
@@ -35,31 +33,23 @@ class ProfileRepository @Inject constructor(
     private val firebaseMessaging: FirebaseMessaging,
     private val dispatchers: CoroutineDispatchers,
 ) {
-    fun getProfile(uid: String, cached: Boolean = false) = flow {
-        emit(State.loading())
-
+    fun getProfile(
+        uid: String,
+        cached: Boolean = false,
+    ) = resultFlow(coroutineContext = dispatchers.io) {
         val source = if (cached) Source.CACHE else Source.DEFAULT
-
         val query = firestore.collectionUsers().whereEqualTo(Field.UID, uid).get(source)
-        val result = query.await()
+        emit(State.success(query.await().toNetworkUser()))
+    }
 
-        emit(State.success(result.toNetworkUser()))
-    }.catch {
-        emit(State.failed(it.message.toString()))
-    }.flowOn(dispatchers.io)
-
-    fun setProfile(uid: String, localUser: LocalUser) = flow {
-        emit(State.loading())
-
+    fun setProfile(uid: String, localUser: LocalUser) = resultFlow(
+        coroutineContext = dispatchers.io,
+    ) {
         val userRef = firestore.collectionUsers().document(uid)
         val token = getFCMToken()
-
         userRef.set(localUser.copy(fcmId = token).toNetworkUser(), SetOptions.merge()).await()
-
         emit(State.success(true))
-    }.catch {
-        emit(State.failed(it.message.toString()))
-    }.flowOn(dispatchers.io)
+    }
 
     private suspend fun getFCMToken(): String {
         return firebaseMessaging.token.await()
