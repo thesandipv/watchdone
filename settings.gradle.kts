fd5d17a3354f7b64a7f70a4269f9e26dbee28c7e
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2025-08-15 17:21:54 +0530

    Update Gradle plugins
    - Update `com.gradle.develocity` to v4.1
    - Update `org.gradle.toolchains.foojay-resolver-convention` to v1.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,8 +26,8 @@ dependencyResolutionManagement {
 }
 
 plugins {
-  id("com.gradle.develocity") version "3.19"
-  id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+  id("com.gradle.develocity") version "4.1"
+  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 develocity {
