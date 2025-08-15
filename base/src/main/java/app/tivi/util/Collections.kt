156dfe2d632647b6998814daa537c2a7c77e9ff7
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2025-08-15 21:51:25 +0530

    Update copyright year and switched to SPDX license identifier

--- base/src/main/java/app/tivi/util/Collections.kt
+++ base/src/main/java/app/tivi/util/Collections.kt
@@ -1,5 +1,7 @@
-// Copyright 2023, Google LLC, Christopher Banes and the Tivi project contributors
-// SPDX-License-Identifier: Apache-2.0
+/*
+ * Copyright 2023, Google LLC, Christopher Banes and the Tivi project contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 package app.tivi.util
 
--- data/src/main/java/app/tivi/data/mappers/Mappers.kt
+++ data/src/main/java/app/tivi/data/mappers/Mappers.kt
@@ -1,5 +1,7 @@
-// Copyright 2018, Google LLC, Christopher Banes and the Tivi project contributors
-// SPDX-License-Identifier: Apache-2.0
+/*
+ * Copyright 2023, Google LLC, Christopher Banes and the Tivi project contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 @file:Suppress("NOTHING_TO_INLINE")
 
